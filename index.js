6be07753772e42c04d2f0e794d8463f653d6c033
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-09-17 11:55:55 +0500

    Add #buildTree function

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
   }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
   if (node.left !== null) {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
 };
+
+function removeDuplicates(array) {
+  return [...new Set(array)];
+}
 
 class Node {
   constructor(value) {
@@ -20,9 +24,24 @@ class Node {
 }
 
 class Tree {
-  constructor(sortedArray) {
+  constructor(array) {
+    const sortedArray = removeDuplicates(array).sort((a, b) => a - b);
     this.root = this.#buildTree(sortedArray);
   }
 
-  #buildTree() {}
+  #buildTree(array) {
+    if (array.length === 0) return null;
+    if (array.length === 1) return new Node(array[0]);
+
+    const mid = Math.ceil(array.length / 2);
+    const root = new Node(array[mid]);
+
+    root.left = this.#buildTree(array.slice(0, mid));
+    root.right = this.#buildTree(array.slice(mid));
+
+    return root;
+  }
 }
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(tree.root);
