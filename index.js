fe1f0cfb37a4a2887867fb92b7f9ef1e861cf3c7
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-09-17 12:30:39 +0500

    Add find function

--- index.js
+++ index.js
@@ -33,13 +33,17 @@ class Tree {
     if (array.length === 0) return null;
     if (array.length === 1) return new Node(array[0]);
 
-    const mid = Math.ceil(array.length / 2);
+    const mid = Math.floor(array.length / 2);
     const root = new Node(array[mid]);
 
     root.left = this.#buildTree(array.slice(0, mid));
     root.right = this.#buildTree(array.slice(mid + 1));
 
     return root;
+  }
+
+  #getNextNode(node, value) {
+    return value < node.value ? node.left : node.right;
   }
 
   insert(value) {
@@ -63,8 +67,43 @@ class Tree {
       }
     }
   }
+
+  //   delete(value) {
+  //     let firstNode = null;
+  //     let secondNode = null;
+  //     let currentNode = this.root;
+  //     while (nextNode.value !== value) {
+  //       secondNode = currentNode;
+  //       currentNode = this.#getNextNode(currentNode, value);
+  //     }
+
+  //     if (!currentNode.left && !currentNode.right) {
+  //       if (value > secondNode.value) {
+  //         secondNode.right = null;
+  //       } else {
+  //         secondNode.left = null;
+  //       }
+  //       return;
+  //     }
+  //     if (!nextNode.left || !nextNode.right) {
+  //       console.log("Has one child");
+  //       currentNode.nextNode = nextNode.nextNode;
+  //       return;
+  //     }
+  //   }
+
+  find(value) {
+    let currentNode = this.root;
+
+    while (true) {
+      console.log(currentNode.value);
+      if (currentNode.value === value) break;
+      currentNode = this.#getNextNode(currentNode, value);
+    }
+    return currentNode;
+  }
 }
 
 const tree = new Tree([1, 2, 3, 4]);
-tree.insert(10);
+console.log(tree.find(3));
 prettyPrint(tree.root);
